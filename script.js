2d64f77abe97cba500abea10159baf9f449e9ae7
NaomiThomson <naomi.thomson@yahoo.com>
2017-04-28 16:37:38 -0700

    basic html and css styling

---
+++ script.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDx-LUA8sZ6DzKHCjQGgbMdTHFuy2pPdms",
+    authDomain: "rps-multiplayer-527c3.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-527c3.firebaseio.com",
+    projectId: "rps-multiplayer-527c3",
+    storageBucket: "rps-multiplayer-527c3.appspot.com",
+    messagingSenderId: "424046617985"
+  };
+  firebase.initializeApp(config);
