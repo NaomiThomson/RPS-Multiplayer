21bc91e7bc16996173c69a9c0e536ccf7adfdc10
NaomiThomson <naomi.thomson@yahoo.com>
2017-05-02 22:25:35 -0700

    added switch statements to det winner of round

--- script3.js
+++ script3.js
@@ -9,6 +9,7 @@ var config = {
 };
 firebase.initializeApp(config);
 
+
 db = firebase.database();
 
 function initDB() {
@@ -37,8 +38,7 @@ db.ref().once("value", function(snapshot) {
   var snapObj = snapshot.val();
 
   try {
-
-    console.log(snapObj.players.p1.name);
+    console.log(snapObj.players);
   }
   catch(e) {
     initDB();
@@ -72,11 +72,84 @@ $('#add-player').click(function() {
 
   if ($('#player1').is(':empty')) {
     db.ref('players/p1').update({name})
-  } else {
+  } else if ($('#player2').is(':empty')) {
     db.ref('players/p2').update({name})
+  } else {
+    alert('Can\'t join right now, sorry!')
   }
 });
 
+
 $('.move1').click(function() {
-  console.log($(this).attr('data-move'));
+  var choice = $(this).attr('data-move');
+  db.ref('players/p1').update({choice})
+});
+
+$('.move2').click(function() {
+  var choice = $(this).attr('data-move');
+  db.ref('players/p2').update({choice});
+});
+
+db.ref().on('child_changed', function(snapshot) {
+  var snapObj = snapshot.val();
+  console.log(snapObj);
+  var p1 = snapObj.p1.choice;
+  var p2 = snapObj.p2.choice;
+  console.log(p1,p2);
+  checkGameResult(p1,p2);
 })
+
+function checkPlayerStatus() {
+  // check to see if other player has chosen yet
+}
+
+function checkGameResult(choice1, choice2) {
+
+  console.log(choice1);
+  console.log(choice2);
+
+  if(choice1==choice2) console.log('Tie!');
+  switch(choice1+choice2){
+    case 'rs': case 'sr':
+      console.log('rock');
+      break
+    case 'rp': case 'pr':
+      console.log('paper');
+      break
+    case 'sp' : case 'ps':
+      console.log('scissors');
+      break
+  };
+
+  // switch (player1Choice) {
+  //
+  //   case player1Choice == player2Choice:
+  //     $('#game-result').html('Tie!');
+  //     break;
+  //
+  //   case player1Choice == 'r' && player2Choice == 'p':
+  //     console.log('p2 wins');
+  //     break;
+  //
+  //   case player1Choice == 'r' && player2Choice == 's':
+  //     console.log('p1 wins');
+  //     break;
+  //
+  //   case player1Choice == 'p' && player2Choice == 'r':
+  //     console.log('p1 wins');
+  //     break;
+  //
+  //   case player1Choice == 'p' && player2Choice == 's':
+  //     console.log('p2 wins');
+  //     break;
+  //
+  //   case player1Choice == 's' && player2Choice == 'p':
+  //     console.log('p1 wins');
+  //     break;
+  //
+  //   case player1Choice == 's' && player2Choice == 'r':
+  //     console.log('p2 wins');
+  //     break;
+  //
+  // }
+}
